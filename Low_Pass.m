90c5ff3202edbea6a58a3f262230a51fab5d90d4
kv13 <kostas33vergo@gmail.com>
2020-04-22 05:26:48 +0300

    -a

--- Low_Pass.m
+++ Low_Pass.m
@@ -9,10 +9,10 @@ function [] = Low_Pass()
 %SPECIFICATIONS
 AEM = [8 5 0 8];
 m = 3;
-f_p = 0.6 * (3+m);
-w_p = 2 * pi * f_p;
-f_s = 2 * f_p;
-w_s = 2* pi * f_s;
+f_p = 0.6 * (3+m)
+w_p = 2 * pi * f_p*1000
+f_s = 2 * f_p
+w_s = 2* pi * f_s*1000
 if(AEM(4)>1)
     a_min = 17.5 + (AEM(4)-5) * 0.5;
 else
@@ -25,10 +25,10 @@ else
 end
 
 %taksi filtorou 
-n = ceil((log((10^(a_min/10)-1)/(10^(a_max/10)-1)))/ (2*log((w_s/w_p))));
+n = ceil((log((10^(a_min/10)-1)/(10^(a_max/10)-1)))/ (2*log((w_s/w_p))))
 
 %sixnotita imisias isxuos 
-w_0 = w_p/((10^(a_max/10)-1)^(1/(2*n)));
+w_0 = w_p/((10^(a_max/10)-1)^(1/(2*n)))
 
 
 y(1) = 0;
@@ -44,9 +44,9 @@ end
 %1h monada
 R_k_1 = 1 ;
 C_k_1 = 1;
-k_f   = w_0;
-k_m   = 10^8 / k_f;
-R_1   = K_m*R_k_1;
+k_f   = w_0
+k_m   = 10^8 / k_f
+R_1   = k_m*R_k_1
 
 
 
