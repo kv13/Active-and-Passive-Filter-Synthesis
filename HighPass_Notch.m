48556b189d12d9e17ffff7c1de8cc18601e2280a
kv13 <kostas33vergo@gmail.com>
2020-05-04 21:23:38 +0300

    HIGH PASS NOTCH FILTER

---
+++ HighPass_Notch.m
@@ -0,0 +1,28 @@
+function [R1 , R2 , R3 , R4 , C, K] = HighPass_Notch(w0,wz,Q)
+%temporary values;
+wt0 = w0/w0;
+wtz = wz/w0;
+
+K1 = wt0^2/wtz - 1; 
+K2 = (2+K1)* Q^2 / ((2+K1)*Q^2+1);
+K  = K2*(wt0^2/wtz^2);
+R1 = 1;
+R3 = 1;
+R2 = Q^2 * (K1 + 2)^2;
+R4 = Q^2 * ( K1 + 2 );
+C  = 1/(Q * (2+K1));
+
+%klimakopoiisi
+k_f = w0;
+%oi piknwtes apo tin ekfonisi tis ergasias exoun sygkekrimeno C=0.1μF
+k_m = 10^7 * C /k_f;
+
+%pragmatika stoixeia
+R1 = k_m * R1;
+R3 = R1;
+R2 = R2 * k_m;
+R4 = R4 * k_m;
+C = 0.1;
+
+end
+
